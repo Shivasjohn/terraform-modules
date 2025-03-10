b94f63947e5fd48677ad4a7d3483242637a2ca82
Shiva John <shiva.john@icf.com>
2025-03-09 22:12:47 -0400

    adding new modules

--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -1,33 +1,5 @@
-resource "aws_security_group" "public_sg" {
-  name        = "public_sg"
-  description = "Security group for public subnet"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "Allow HTTP traffic"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow HTTPS traffic"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "public_sg"
-  }
+module "security_group" {
+  source = "git::https://github.com/Shivasjohn/terraform-modules.git//modules/security-group?ref=main"
+  vpc_id = module.vpc.vpc_id
+  tags   = var.tags
 }
