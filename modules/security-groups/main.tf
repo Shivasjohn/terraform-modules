56c82c92835dd7bca955414389aa6df649a336bd
Shiva John <shiva.john@icf.com>
2025-03-09 22:17:14 -0400

    adding new modules

--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -1,5 +1,15 @@
-module "security_group" {
-  source = "git::https://github.com/Shivasjohn/terraform-modules.git//modules/security-group?ref=main"
-  vpc_id = module.vpc.vpc_id
-  tags   = var.tags
+resource "aws_security_group" "this" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.tags, {
+    Name = "default-security-group"
+  })
 }
+
